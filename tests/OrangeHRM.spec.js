a65b3a0f1c0d8911df2c444ce7d5ad1f776ec830
Jonathan Vela cadena <jonathanv@Jonathans-MacBook-Air.local>
2025-04-22 19:09:56 -0500

    New project Orange HRM

---
+++ tests/OrangeHRM.spec.js
@@ -0,0 +1,59 @@
+const {test,expect}= require('@playwright/test');
+
+
+
+test.only ('Browser context playwright test', async ({browser})=>
+
+{
+const context= await browser.newContext();
+const page = await context.newPage();
+await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+
+});
+
+
+test ('Error message test', async ({page})=>
+
+    {
+   //const context= await browser.newContext();h
+   //const page = await context.newpage();
+    await page.goto("https://www.saucedemo.com");
+
+   // get-tittle assertion
+   console.log (await page.title());
+   await expect(page).toHaveTitle("Swag Labs");
+    //css xpath
+    await page.locator("#user-name").type("standard_user");
+    await page.locator("#password").type("wisin");
+    await page.locator("#login-button").click();
+    console.log(await page.locator("h3[data-test='error']").textContent());
+    await expect(page.locator("h3[data-test='error']")).toHaveText("Epic sadface: Username and password do not match any user in this service"); 
+
+    });
+
+
+test('Sucesfull login', async ({page})=>
+
+        {
+       //const context= await browser.newContext();h
+       //const page = await context.newpage();
+        const cardTitles = page.locator(".inventory_item_name");
+        await page.goto("https://www.saucedemo.com");
+    
+       // get-tittle assertion
+       console.log (await page.title());
+       await expect(page).toHaveTitle("Swag Labs");
+        //css xpath
+        await page.locator("#user-name").type("standard_user");
+        await page.locator("#password").type("secret_sauce");
+        await page.locator("#login-button").click();
+        console.log(await page.locator("//div[@class='app_logo']").textContent());
+        await expect(page.locator("//div[@class='app_logo']")).toHaveText("Swag Labs"); 
+
+       // const allTitle = await cardTitles.allTextcontent();
+        console.log(await cardTitles.first().textContent());
+        console.log(await cardTitles.nth(1).textContent());
+        const allTitle = await cardTitles.allTextContents();
+        console.log(allTitle);
+        });
+
