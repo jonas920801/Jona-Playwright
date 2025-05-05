e2ab363242d5b47a7fdac0c9038f0ffed645c7f1
Jonathan Vela cadena <jonathanv@Jonathans-MacBook-Air.local>
2025-05-05 13:41:40 -0500

    Orange Project PIM Module

--- tests/OrangeHRM.spec.js
+++ tests/OrangeHRM.spec.js
@@ -39,7 +39,7 @@ test('Error message login', async ({page})=>
 
 
 
-test.only('Successfully login', async ({page})=>
+test('Successfully login', async ({page})=>
 
     {
    
@@ -60,3 +60,39 @@ test.only('Successfully login', async ({page})=>
 
 
 // npx playwright test tests/OrangeHRM.spec.js --debug  ----- command to run the test in debug mode
+
+
+test.only('PIM Module', async ({page})=>
+
+    {
+   
+    await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+
+   // get-tittle tab assertion 
+   console.log (await page.title());
+   await expect(page).toHaveTitle("OrangeHRM");
+
+    //css xpath loginng feature
+    await page.locator("input[placeholder='Username']").type("Admin");
+    await page.locator("//input[@placeholder='Password']").type("admin123");
+    await page.locator("button[type='submit']").click();
+
+    // Check logo
+    await expect(page.locator('img[alt="client brand banner"]')).toBeVisible();
+
+    // Click on PIM module
+    await page.locator("//span[@class='oxd-text oxd-text--span oxd-main-menu-item--name'][normalize-space()='PIM']").click();
+
+    // add employee
+    await page.locator("//button[normalize-space()='Add']").click();
+    await page.locator("input[placeholder='First Name']").type("Jonathan");
+    await page.locator("input[placeholder='Middle Name']").type("Test");
+    await page.locator("input[placeholder='Last Name']").type("Test 2");
+    await page.locator("button[type='submit']").click();
+    
+   
+    });
+
+
+
+
