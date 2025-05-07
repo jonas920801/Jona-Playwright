a4a6c9f38f1ec86cd47e2dfa1b1a613709b638af
Jonathan Vela cadena <jonathanv@Jonathans-MacBook-Air.local>
2025-05-07 18:33:51 -0500

    Orange Project add switch method

--- tests/OrangeHRM.spec.js
+++ tests/OrangeHRM.spec.js
@@ -73,23 +73,29 @@ test.only('PIM Module', async ({page})=>
    await expect(page).toHaveTitle("OrangeHRM");
 
     //css xpath loginng feature
-    await page.locator("input[placeholder='Username']").type("Admin");
-    await page.locator("//input[@placeholder='Password']").type("admin123");
+    await page.locator("input[placeholder='username']").type("Admin");
+    await page.locator("input[placeholder='password']").type("admin123");
     await page.locator("button[type='submit']").click();
 
     // Check logo
     await expect(page.locator('img[alt="client brand banner"]')).toBeVisible();
 
     // Click on PIM module
-    await page.locator("//span[@class='oxd-text oxd-text--span oxd-main-menu-item--name'][normalize-space()='PIM']").click();
+    await page.locator("//span[normalize-space()='PIM']").click();
 
     // add employee
     await page.locator("//button[normalize-space()='Add']").click();
-    await page.locator("input[placeholder='First Name']").type("Jonathan");
-    await page.locator("input[placeholder='Middle Name']").type("Test");
-    await page.locator("input[placeholder='Last Name']").type("Test 2");
-    await page.locator("button[type='submit']").click();
-    
+    await page.locator("//input[@placeholder='First name']").type("Jonathan");
+    await page.locator("//input[@placeholder='Middle name']").type("Test");
+    await page.locator("//input[@placeholder='Last Name']").type("Test 2");
+
+   //Enable the switch button
+    await page.check("//span[@class='oxd-switch-input oxd-switch-input--active --label-right']");
+
+   //
+    //await page.locator("button[type='submit']").click();
+    await page.locator("(//input[@class='oxd-input oxd-input--active'])[3]").type("jontest1");
+
    
     });
 
