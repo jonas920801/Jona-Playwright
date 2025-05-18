ede1594c55000ad04b460f874371cb214ac27f9a
Jonathan <jonathanv@Jonathans-MacBook-Air.local>
2025-05-18 11:41:55 -0500

    Demo QA Element text box

--- tests/demoqa.spec.js
+++ tests/demoqa.spec.js
@@ -12,7 +12,7 @@ await page.goto('https://demoqa.com/');
 });
 
 
-test.only('Go to Demoqa Successfuly', async ({page})=>
+test('Go to Demoqa Successfuly', async ({page})=>
 
     {
    
@@ -22,5 +22,32 @@ test.only('Go to Demoqa Successfuly', async ({page})=>
    console.log (await page.title());
    await expect(page).toHaveTitle("DEMOQA");
 
-
     });
+
+
+
+test.only('Element Text box ', async ({page})=>
+
+        {
+       
+        await page.goto("https://demoqa.com/");
+    
+       // get-tittle tab assertion 
+       console.log (await page.title());
+       await expect(page).toHaveTitle("DEMOQA");
+
+       await page.locator("//div[@class='category-cards']//div[1]//div[1]//div[2]//*[name()='svg']").click();
+       await page.locator("//span[normalize-space()='Text Box']").click();
+       await page.locator("//input[@id='userName']").type("Jonathan");
+       await page.locator("//input[@id='userEmail']").type("jonas123@gmail.com");
+       await page.locator("//textarea[@id='currentAddress']").type("1234 Main St");
+       await page.locator("//textarea[@id='permanentAddress']").type("768 Brooklyn St");
+       await page.locator(" //button[@id='submit']").click();
+        });
+
+
+
+
+
+
+    //npx playwright codegen http://google.com  run this command in terminal to generate code for the test
