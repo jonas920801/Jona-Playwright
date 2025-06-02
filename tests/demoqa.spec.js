21f2d01994951789a6766b1f88e4155bf9a005da
Jonathan <jonathanv@Jonathans-MacBook-Air.local>
2025-06-02 17:29:46 -0500

    Demo QA Radio Button

--- tests/demoqa.spec.js
+++ tests/demoqa.spec.js
@@ -48,7 +48,7 @@ test('Element Text box ', async ({page})=>
 
 
 
-test.only('Element Check box ', async ({page})=>
+test('Element Check box ', async ({page})=>
 
             {
            
@@ -73,9 +73,41 @@ test.only('Element Check box ', async ({page})=>
            const isChecked = await page.locator('#tree-node-home').isChecked();
            console.log('Is Home checked?', isChecked);
 
+                  // Click the "Desktop" label to toggle the checkbox
+           if (!isChecked) {
+                console.log('The checkbox is NOT checked.');
+                // You can also click to check it if needed
+                await page.locator('label[for="tree-node-home"]').click();
+              }
+
+           
+
             });
 
 
+test.only('Radio button ', async ({page})=>
 
+              {
+             
+              await page.goto("https://demoqa.com/");
+          
+             // get-tittle tab assertion 
+             console.log (await page.title());
+             await expect(page).toHaveTitle("DEMOQA");
+             await page.locator("//div[@class='category-cards']//div[1]//div[1]//div[2]//*[name()='svg']").click();
+             await page.locator("//span[normalize-space()='Radio Button']").click();
+
+
+            // Click the "Impressive" radio button using the label text
+            await page.locator("//label[@for='impressiveRadio']").click();
+
+
+            // Optional: verify that it was selected
+            const isChecked = await page.locator('input#impressiveRadio').isChecked();
+            console.log('Is "Impressive" selected?', isChecked); // should be true
+            
+
+          
+          });
 
     //npx playwright codegen http://google.com  run this command in terminal to generate code for the test
